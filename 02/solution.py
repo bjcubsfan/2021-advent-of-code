80ccd0fe0ed280281e658a6fe0e49e208a0beeba
B. J. Potter <bj.ctr.potter@faa.gov>
2021-12-02 00:09:58 -0600

    Solve day 2 part 2

--- 02/solution.py
+++ 02/solution.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
 
+def advanced_update_position(aim, horizontial, depth, instruction):
+    token, number = instruction.split()
+    number = int(number.strip())
+    if token == "forward":
+        horizontial += number
+        depth += aim * number
+    elif token == "up":
+        aim -= number
+    elif token == "down":
+        aim += number
+    return aim, horizontial, depth
+
+
 def update_position(horizontial, depth, instruction):
     token, number = instruction.split()
     number = int(number.strip())
@@ -22,11 +35,21 @@ def position_multiplied(input_data):
     return horizontal * depth
 
 
+def advanced_position_multiplied(input_data):
+    aim = 0
+    horizontal = 0
+    depth = 0
+    input_data = input_data.strip()
+    for line in input_data.split("\n"):
+        aim, horizontal, depth = advanced_update_position(aim, horizontal, depth, line)
+    return horizontal * depth
+
+
 def main():
     with open("input") as input_file:
         input_data = input_file.read()
     print(position_multiplied(input_data))
-    # print(sliding_num_deeper(input_data))
+    print(advanced_position_multiplied(input_data))
 
 
 if __name__ == "__main__":
--- 02/test_solution.py
+++ 02/test_solution.py
@@ -1,4 +1,4 @@
-from solution import position_multiplied
+from solution import advanced_position_multiplied, position_multiplied
 
 input_data = """forward 5
 down 5
@@ -11,3 +11,8 @@ forward 2"""
 def test_position_multiplied():
     calc_pos_mult = position_multiplied(input_data)
     assert calc_pos_mult == 150
+
+
+def test_advanced_position_multiplied():
+    calc_pos_mult = advanced_position_multiplied(input_data)
+    assert calc_pos_mult == 900
