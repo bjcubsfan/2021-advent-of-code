3270f951de1f3f6d97d92d04e8670b460b3453c6
B. J. Potter <bj.ctr.potter@faa.gov>
2021-12-12 00:01:58 -0600

    solve day 6 part 1

--- 06/solution.py
+++ 06/solution.py
@@ -3,16 +3,33 @@
 from collections import defaultdict
 from pprint import pprint
 
-def simulate_fish(value, days):
-    return value + days
+HIGHEST_FISH_DAYS = 8
+RESET_TO = 6
+
+def simulate_day(fish_map):
+    new_fish_map = {num: 0 for num in range(HIGHEST_FISH_DAYS + 1)}
+    for at_day in range(HIGHEST_FISH_DAYS + 1):
+        number = fish_map[at_day]
+        if at_day == 0:
+            new_fish_map[HIGHEST_FISH_DAYS] += number
+            new_fish_map[RESET_TO] += number
+        else:
+            new_fish_map[at_day - 1] += number
+    return new_fish_map
+
+
+def simulate_days(fish_map, days):
+    for _ in range(days):
+        fish_map = simulate_day(fish_map)
+    return sum(fish_map.values())
 
 def part_1(input_data, days):
     input_data = input_data.strip()
-    answer = None
     total_fish = 0
+    fish_map = {num: 0 for num in range(HIGHEST_FISH_DAYS + 1)}
     for fish in input_data.split(","):
-        final_fish = simulate_fish(int(fish), days)
-        total_fish += final_fish
+        fish_map[int(fish)] += 1
+    answer = simulate_days(fish_map, int(days))
     return answer
 
 def part_2(input_data):
@@ -25,7 +42,7 @@ def part_2(input_data):
 def main():
     with open("input") as input_file:
         input_data = input_file.read()
-    print(part_1(input_data, 3))
+    print(part_1(input_data, 80))
     print(part_2(input_data))
 
 
